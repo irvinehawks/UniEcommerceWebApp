1d028a35b6edbe82dc10a0ff7c04e4fa740be349
Irvene Kwambana <irvenehawks@gmail.com>
2022-10-16 05:11:53 +0200

    database config setup

--- backend/utils/generateToken.js
+++
@@ -1,9 +0,0 @@
-import jwt from 'jsonwebtoken'
-
-const generateToken = async() => {
-    return jwt.sign({ id }, process.env.JWT_SECRET, {
-        expiresIn: '5d'
-    })
-}
-
-export default generateToken
